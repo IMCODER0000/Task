793862df6c5ea973e0cc5803e1f435d9d5a80b26
IMCODER0000 <gustn9025@naver.com>
2025-05-26 14:48:15 +0900

    finish

--- ShoppingMall.java
+++ ShoppingMall.java
@@ -7,13 +7,11 @@ import java.util.function.Supplier;
 public class ShoppingMall {
     private static Map<Class<? extends Fruit>, Fruit> registry = new HashMap<>();
 
-    // 과일 등록
     public static void register(Class<? extends Fruit> clazz, Fruit fruit) {
         registry.put(clazz, fruit);
     }
 
 
-    // 등록된 과일 인스턴스 반환
     public static Fruit getFruit(Fruit fruit) {
         Fruit fruit1 = registry.get(fruit.getClass());
         if (fruit1 != null) {
